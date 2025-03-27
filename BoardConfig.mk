566d5aaa4380488a44baa1d2eedb587dd6f89626
Anirudh Menon <thenasaplusit@proton.me>
2025-03-27 21:19:31 +0100

    add twrp support
    Change-Id: I757c44dcb21955c9250b1ea16d09d199e986e106

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,13 +19,10 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 2236070912
 BOARD_CACHEIMAGE_PARTITION_SIZE := 573741824
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Recovery
-BOARD_LDPI_RECOVERY := true
-ifeq ($(TWRP_BUILD),true)
--include device/samsung/twrp-common/twrp.mk
-else
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.rhea_ss_zanin
-endif
-
 # inherit from the proprietary version
 -include vendor/samsung/zanin/BoardConfigVendor.mk
+
+#twrp
+RECOVERY_VARIANT := twrp
+RECOVERY_SDCARD_ON_DATA := true
+TW_THEME := watch_mdpi
