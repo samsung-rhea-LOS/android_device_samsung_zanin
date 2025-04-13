116b2e39150145d5450d1898b2f4896c99a9b162
Anirudh Menon <thenasaplusit@proton.me>
2025-04-13 11:53:25 +0200

    fix references to corsica I missed

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,33 +3,33 @@ LOCAL_PATH:= $(call my-dir)
 # Init scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.rhea_ss_corsica
+LOCAL_MODULE       := fstab.rhea_ss_zanin
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.rhea_ss_corsica
+LOCAL_SRC_FILES    := etc/fstab.rhea_ss_zanin
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.rhea_ss_corsica.rc
+LOCAL_MODULE       := init.recovery.rhea_ss_zanin.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.rhea_ss_corsica.rc
+LOCAL_SRC_FILES    := etc/init.recovery.rhea_ss_zanin.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.rhea_ss_corsica.rc
+LOCAL_MODULE       := init.rhea_ss_zanin.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.rhea_ss_corsica.rc
+LOCAL_SRC_FILES    := etc/init.rhea_ss_zanin.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.rhea_ss_corsica.rc
+LOCAL_MODULE       := ueventd.rhea_ss_zanin.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/ueventd.rhea_ss_corsica.rc
+LOCAL_SRC_FILES    := etc/ueventd.rhea_ss_zanin.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
